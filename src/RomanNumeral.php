fe0b512d268f1462450c401dd155ece0e4867a54
Amran-Lab <amran3210@gmail.com>
2021-01-03 20:19:09 +0000

    first commit

---
+++ src/RomanNumeral.php
@@ -0,0 +1,59 @@
+<?php
+namespace PhpNwSykes;
+use PhpNwSykes\InvalidNumeral;
+class RomanNumeral
+{
+    protected $symbols = [
+        1000 => 'M',
+        500 => 'D',
+        100 => 'C',
+        50 => 'L',
+        10 => 'X',
+        5 => 'V',
+        1 => 'I',
+    ];
+
+    protected $numeral;
+
+    public function __construct(string $romanNumeral)
+    {
+        $this->numeral = $romanNumeral;
+    }
+
+    /**
+     * Converts a roman numeral such as 'X' to a number, 10
+     *
+     * @throws InvalidNumeral on failure (when a numeral is invalid)
+     */
+    public function toInt():int
+    {
+        $total = 0;
+        $key = 0;
+        $stuff = str_split($this->numeral);                    #turns numeral string into array of characters
+        
+        foreach ($stuff as &$value1) {                              #going through every charcter in numeral
+            if (in_array($value1, $this->symbols))          #checks is character is in array of symbols if not throws exception
+                { } 
+            else
+            { 
+                throw new InvalidNumeral; 
+            } 
+            foreach ($this->symbols as &$value) {                     #goes through every symbol to check for a match
+            
+                if( $value1 == $value ){
+                    $key_prev = $key;
+                    $key = array_search($value, $this->symbols);                   #Gets index from value
+                    
+                    
+                    if( $key_prev < $key && $key_prev != 0){                #if the previous value is lower than current value take away e.g I then X 
+                        $total -= $key_prev;                                       # take I or 1 away from total twice due to counteracting the addition the previous time
+                        $total -= $key_prev;
+                        
+                    }
+                $total += $key;
+            }
+        }
+        }
+        return $total;
+    }
+}
